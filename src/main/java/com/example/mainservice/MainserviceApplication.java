4f3315f65b8c5dd63c4d04c1ef64c57b40f59973
Kavindi487 <kavindividusari@gmail.com>
2026-01-14 14:49:29 +0530

    updated whole emergency backend

--- src/main/java/com/example/mainservice/MainserviceApplication.java
+++ src/main/java/com/example/mainservice/MainserviceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainserviceApplication {
+public class  MainserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainserviceApplication.class, args);
