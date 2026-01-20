eddaf3e7afedbb358ed8005b063054c0dc617843
VENITH <152465117+VenithAbey@users.noreply.github.com>
2026-01-20 19:20:15 +0530

    Update LoginOtpSessionRepository.java

--- src/main/java/com/example/mainservice/repository/LoginOtpSessionRepository.java
+++ src/main/java/com/example/mainservice/repository/LoginOtpSessionRepository.java
@@ -2,11 +2,14 @@ package com.example.mainservice.repository;
 
 import com.example.mainservice.entity.LoginOtpSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface LoginOtpSessionRepository extends JpaRepository<LoginOtpSession, Long> {
     Optional<LoginOtpSession> findBySessionId(String sessionId);
+
+    @Transactional
     void deleteByPatientId(Long patientId);
 }
 
